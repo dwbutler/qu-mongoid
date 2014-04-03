d102eaa7a88498beb21a29289b7ce80a0c81e4ca
David Butler <dwbutler@ucla.edu>
2014-04-03 07:58:22 -0700

    Convert specs to RSpec 2.14.8 syntax with Transpec
    This conversion is done by Transpec 1.10.4 with the following command:
        transpec
    
    * 12 conversions
        from: obj.should
          to: expect(obj).to
    
    * 6 conversions
        from: == expected
          to: eq(expected)
    
    * 2 conversions
        from: lambda { }.should_not
          to: expect { }.not_to
    
    * 1 conversion
        from: Klass.any_instance.should_receive(:message)
          to: expect_any_instance_of(Klass).to receive(:message)
    
    * 1 conversion
        from: obj.should_receive(:message)
          to: expect(obj).to receive(:message)

--- spec/qu/backend/mongoid_spec.rb
+++ spec/qu/backend/mongoid_spec.rb
@@ -5,27 +5,27 @@ describe Qu::Backend::Mongoid do
   it_should_behave_like 'a backend'
 
   describe 'connection' do
-    it 'should use a separate connection in each thread' do
+    it 'uses a separate connection in each thread' do
       backend = subject
       connection = subject.connection
       separate_connection = false
       Thread.new do
         separate_connection = (backend.connection != connection)
       end.join
-      separate_connection.should be_true
+      expect(separate_connection).to be_true
     end
     
-    it 'should default to the qu database' do
-      subject.connection.should be_instance_of(Moped::Session)
-      subject.connection.options[:database].should == 'qu'
+    it 'defaults to the qu database' do
+      expect(subject.connection).to be_instance_of(Moped::Session)
+      expect(subject.connection.options[:database]).to eq('qu')
     end
     
-    it 'should default to the :default session' do
-      subject.session.should == :default
-      subject.connection.should == ::Mongoid::Sessions.with_name(subject.session)
+    it 'defaults to the :default session' do
+      expect(subject.session).to eq(:default)
+      expect(subject.connection).to eq(::Mongoid::Sessions.with_name(subject.session))
     end
     
-    it 'should have a configurable session that works with threads' do
+    it 'has a configurable session that works with threads' do
       subject.connection = nil
       
       ::Mongoid.sessions[:qu] = {:uri => 'mongodb://127.0.0.1:27017/quspec', :max_retries_on_connection_failure => 4}
@@ -33,29 +33,29 @@ describe Qu::Backend::Mongoid do
       Qu.configure do |c|
         c.backend.session = :qu
       end
-      subject.connection.should == ::Mongoid::Sessions.with_name(:qu)
+      expect(subject.connection).to eq(::Mongoid::Sessions.with_name(:qu))
       
       should_have_qu_session_in_new_thread = false
       Thread.new do
         should_have_qu_session_in_new_thread = (subject.connection == ::Mongoid::Sessions.with_name(:qu))
       end.join
-      should_have_qu_session_in_new_thread.should be_true
+      expect(should_have_qu_session_in_new_thread).to be_true
       
       # Clean up
       subject.connection=nil
       ::Mongoid.connect_to('qu')
     end
     
-    it 'should use MONGOHQ_URL from heroku' do
+    it 'uses MONGOHQ_URL from heroku' do
       # Clean up from other tests
       ::Mongoid.sessions[:default]  = nil
       subject.connection=nil
       ::Mongoid::Sessions.clear
       
       ENV['MONGOHQ_URL'] = 'mongodb://127.0.0.1:27017/quspec'
-      subject.connection.options[:database].should == 'quspec'
-      subject.connection.cluster.nodes.first.resolved_address.should == "127.0.0.1:27017"
-      ::Mongoid.sessions[:default][:hosts].should include("127.0.0.1:27017")
+      expect(subject.connection.options[:database]).to eq('quspec')
+      expect(subject.connection.cluster.nodes.first.resolved_address).to eq("127.0.0.1:27017")
+      expect(::Mongoid.sessions[:default][:hosts]).to include("127.0.0.1:27017")
       
       # Clean up MONGOHQ stuff
       ENV.delete('MONGOHQ_URL')
@@ -68,18 +68,18 @@ describe Qu::Backend::Mongoid do
     let(:worker) { Qu::Worker.new }
 
     describe "on mongo >=2" do
-      it 'should return nil when no jobs exist' do
+      it 'returns nil when no jobs exist' do
         subject.clear
-        Moped::Session.any_instance.should_receive(:command).and_return(nil)
-        lambda { subject.reserve(worker, :block => false).should be_nil }.should_not raise_error
+        expect_any_instance_of(Moped::Session).to receive(:command).and_return(nil)
+        expect { expect(subject.reserve(worker, :block => false)).to be_nil }.not_to raise_error
       end
     end
 
     describe 'on mongo <2' do
-      it 'should return nil when no jobs exist' do
+      it 'returns nil when no jobs exist' do
         subject.clear
-        subject.connection.should_receive(:command).and_raise(Moped::Errors::OperationFailure.new(nil, 'test'))
-        lambda { subject.reserve(worker, :block => false).should be_nil }.should_not raise_error
+        expect(subject.connection).to receive(:command).and_raise(Moped::Errors::OperationFailure.new(nil, 'test'))
+        expect { expect(subject.reserve(worker, :block => false)).to be_nil }.not_to raise_error
       end
     end
   end
